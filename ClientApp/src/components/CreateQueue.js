e20857fbb4cb91d8574b2cc1a3e3d792e75b3f1a
Dima Chornobai <91607257+Chornobai22@users.noreply.github.com>
2022-02-07 21:16:45 +0200

    Update CreateQueue.js

--- ClientApp/src/components/CreateQueue.js
+++ ClientApp/src/components/CreateQueue.js
@@ -1,1 +1,32 @@
-﻿
+import React, { Component } from 'react';
+import { Collapse, Container, Navbar, NavbarBrand, NavbarToggler, NavItem, NavLink } from 'reactstrap';
+import { Link } from 'react-router-dom';
+import './CreateQueue.css';
+
+export class CreateQueue extends Component {
+    static displayName = CreateQueue.name;
+
+    render() {
+        return (
+            <div class="flexbox">
+                <div class="flexboxvert">
+                    <form class="item" action="">
+                        <input type="text" class="iteminput" placeholder="Name your queue" />
+                    </form>
+                    <form class="item" action="">
+                        <input type="text" class="iteminput1" placeholder="Queue description" />
+                    </form>
+                    <form class="item" action="">
+                        <input type="text" class="iteminput" placeholder="Password" />
+                    </form>
+                </div>
+                <div>
+                    <form action="">
+                        <input class="formbutton" type="submit" value="Create queue" />
+                    </form>
+                </div>
+
+            </div>
+        );
+    }
+}
