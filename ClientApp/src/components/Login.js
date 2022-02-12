1d3c159a8dd3032109e620b0e492fb4605639566
mykytynsofia <mykytynsofia@ukr.net>
2022-02-12 18:46:50 +0200

    login style added, need some fixes

--- ClientApp/src/components/Login.js
+++ ClientApp/src/components/Login.js
@@ -1,6 +1,7 @@
 ﻿import React, { Component } from 'react';
-import {Input} from 'reactstrap';
+import { Container, Row, Col, Input } from 'reactstrap';
 import './Login.css';
+import { Link } from "react-router-dom";
 
 export class Login extends Component {
     static displayName = Login.name;
@@ -26,37 +27,72 @@ export class Login extends Component {
         const requestOptions = {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
-            /*body: JSON.stringify({email: this.state.email, password: this.state.password}) */
-            body: JSON.stringify({email: "qqq12", password: "12312"}) 
+            body: JSON.stringify({ email: this.state.email, password: this.state.password })
+            /*body: JSON.stringify({email: "qqq12", password: "12312"})*/
         };
 
         const response = await fetch('/login', requestOptions)
         const data = await response.text();
         sessionStorage.setItem('token', data);
-
         sessionStorage.setItem('email', this.state.email);
         window.open("/", "_self");
     }
 
     render() {
-        return (
-            
-            <div class = "flexbox">
-              <div class="flexboxvert">
-                  <div className="input_email">
-                    <Input type="text" className="iteminput" placeholder ="E-mail" value={this.state.email} onChange={this.handleEmailChange} />
-                  </div>
-                  <form class="item" action="">
-                    <Input type="text" className="iteminput" placeholder ="Password" value={this.state.password} onChange={this.handlePasswordChange} />
-                    {/*<input type="text" class="iteminput "placeholder = "Password" />*/}
-                  </form>
-               </div>
-               <div class="flexboxvert1">
-                    <a href="/createqueue" className="btn btn-1 btn-sep icon-info" onClick={this.handleSubmit}>Login</a>
-                    <a href="/forgotpass" className="btn2 forgotpass1">Forgot password?</a>
-               </div>
-         
-            </div>
+        
+            return (
+                <Container fluid>
+                    <div className="mainbox">
+
+                        <Col >
+                            <div className="inputitem">
+
+                                <div  className="google_btn" >
+                                    Sign in with Google 
+                                </div>
+
+                                <div className="inputbox">
+
+                                    <Input type="text" value={this.state.email} onChange={this.handleEmailChange} placeholder="E-mail" />
+                                </div>
+
+                                <div className="inputbox">
+
+                                    <Input type="password" value={this.state.password} onChange={this.handlePasswordChange} placeholder="Password" />
+
+                                </div>
+
+                            </div>
+
+                            
+                        </Col>
+                        <Col >
+                            <Row className="btns">
+                                <Col sm="6" className="btn">
+                                    <div className="remember_btn" >Remember me
+                                        </div>
+                                </Col>
+                                <Col sm="6" className="btn">
+                                    <div > 
+                                        <a href="/createqueue" className="login_btn" onClick={this.handleSubmit}>LogIn</a>
+                                    </div>
+                                </Col>
+                                <Col sm="6" className="btn">
+                                    <div className="register_btn">Register now</div>
+                                </Col>
+                                <Col sm="6" className="btn">
+                                    <div >
+                                        <a href="/forgotpass" className="forgotpass_btn" >Forgot password?</a>
+                                    </div>
+                                </Col>
+                            </Row>
+                        </Col>
+
+                       
+
+
+                    </div>
+                </Container>
         );
     }
 }
