fc54e4c603e3bd7b92972d010bce176695dafc4d
YuriyPukhta <yuriy.pukhta@gmail.com>
2022-02-18 19:21:07 +0200

    add email servise send

---
+++ Service/EmailService.cs
@@ -0,0 +1,62 @@
+using QuiQue.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Options;
+using MimeKit;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Microsoft.AspNetCore.Mvc;
+using MailKit.Net.Smtp;
+
+
+
+namespace QuiQue.Service
+{
+   public interface IEmailSender
+   {
+            Task<string> SendEmailAsync(string recipientEmail, string recipientFirstName, string Link);
+   }
+    public class EmailSenderService : IEmailSender
+    {
+        private readonly SmtpSettings _smtpSettings;
+        public EmailSenderService(IOptions<SmtpSettings> smtpSettings)
+        {
+            _smtpSettings = smtpSettings.Value;
+        }
+
+        public async Task<string> SendEmailAsync(string recipientEmail, string recipientFirstName, string Link)
+        {
+            var message = new MimeMessage();
+            message.From.Add(MailboxAddress.Parse(_smtpSettings.SenderEmail));
+            message.To.Add(MailboxAddress.Parse(recipientEmail));
+            message.Subject = "How to send email in .Net Core";
+            message.Body = new TextPart("plain")
+            {
+                Text = "This is just a walkthrough in sending messages in .net core"
+            };
+
+            var client = new SmtpClient();
+
+            try
+            {
+                await client.ConnectAsync(_smtpSettings.Server, _smtpSettings.Port, true);
+                await client.AuthenticateAsync(new NetworkCredential(_smtpSettings.SenderEmail, _smtpSettings.Password));
+                await client.SendAsync(message);
+                await client.DisconnectAsync(true);
+                return "Email Sent Successfully";
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                client.Dispose();
+            }
+        }
+    }
+}
